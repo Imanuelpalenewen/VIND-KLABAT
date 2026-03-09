a3f7757c9b3c14f19312c230fbf621c42fa85765
Imanuel P <imanuelpalenewen15.com>
2026-03-09 23:05:00 +0800

    fix:minor changes

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type * as data_consultations from "../data/consultations.js";
 import type * as data_courses from "../data/courses.js";
 import type * as data_grades from "../data/grades.js";
 import type * as data_lecturers from "../data/lecturers.js";
+import type * as data_news from "../data/news.js";
 import type * as data_schedules from "../data/schedules.js";
 import type * as data_students from "../data/students.js";
 import type * as enrollment from "../enrollment.js";
@@ -35,6 +36,7 @@ declare const fullApi: ApiFromModules<{
   "data/courses": typeof data_courses;
   "data/grades": typeof data_grades;
   "data/lecturers": typeof data_lecturers;
+  "data/news": typeof data_news;
   "data/schedules": typeof data_schedules;
   "data/students": typeof data_students;
   enrollment: typeof enrollment;
